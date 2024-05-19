4716daa3707059e9394091e88b9bc4ed5a8c74e5
Holmeister73 <122776908+Holmeister73@users.noreply.github.com>
2024-05-19 17:31:58 +0300

    Update train_bash.py

--- containers/turkish-lmtuner-hpc/turkish-lm-tuner-fork/turkish_lm_tuner/train_bash.py
+++ containers/turkish-lmtuner-hpc/turkish-lm-tuner-fork/turkish_lm_tuner/train_bash.py
@@ -188,7 +188,6 @@ if __name__ == "__main__":
             #results_df.to_csv(str(run_name)+"_"+str(lr)+"_results"+str(i)+".csv", index = False)
             
     elif task_format == "conditional_generation":
-        learning_rates["mBART"] = [4e-6, 5e-6, 2e-5]
         for i in range(1):  #normalde burası 3 olacak 3 run için
           for lr in learning_rates[model_keyword][1:2]:  #normalde burada [1:2] olmayacak farklı learning rateler için
             optimizer_params["lr"] = lr
